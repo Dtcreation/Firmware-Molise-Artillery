7f940a8fb9aa4561dc78516735f66475f2b80d99
Molise <60579620+Dtcreation@users.noreply.github.com>
2021-02-11 16:20:12 +0100

    Revert "change x_min_pos for TouchMi"

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1490,17 +1490,17 @@
 
 
 #ifdef GENIUS
-  #define X_BED_SIZE 220
+  #define X_BED_SIZE 226
   #define Y_BED_SIZE 226
 #endif
 #ifdef X1
-  #define X_BED_SIZE 300
+  #define X_BED_SIZE 308
   #define Y_BED_SIZE 308
 #endif
 
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -5
+#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
