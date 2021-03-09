40e65e0c9c6087ff5886a627c3e93702a442ec72
Dtcreation <dtcreation3d@icloud.com>
2021-03-09 09:33:37 +0100

    hex

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -90,16 +90,16 @@
 
 /*** Section 1 Artillery Printer ***/
 
-//#define GENIUS
-#define X1
+#define GENIUS
+//#define X1
 
 /*** Section 2 Board Type ***/
 
-//#define MKSGENL         // Stock Board
+#define MKSGENL         // Stock Board
 //#define MKSGENLV21      // Choose this if you are using MKS GEN L V2.1
 //#define SKR13           // Choose this if you are using BigTreeTech SKR 1.3
 //#define SKR14           // Choose this if you are using BigTreeTech SKR 1.4
-#define SKR14T          // Choose this if you are using BigTreeTech SKR 1.4 Turbo
+//#define SKR14T          // Choose this if you are using BigTreeTech SKR 1.4 Turbo
 //#define MKSSGENLV1      // Choose this if you are using MKS SGEN L V1
 //#define MKSSGENLV2      // Choose this if you are using MKS SGEN L V2
 
@@ -123,12 +123,12 @@
 
 /*** Section 4 Drivers Type ***/
 
-//#define TMC_2100        // Stock Drivers
+#define TMC_2100        // Stock Drivers
 //#define TMC_2208_STA    // Standalone Mode
 //#define TMC_2209_STA    // Standalone Mode
 //#define LV_8729
 //#define TMC_2208        // UART Mode
-#define TMC_2209        // UART Mode
+//#define TMC_2209        // UART Mode
 
 /*** Section 5 Bed Leveling ***/
 
