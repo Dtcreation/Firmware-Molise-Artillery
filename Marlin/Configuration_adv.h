9fb15c351cf59ef1b90f8ad7604bc879de2f4804
Dtcreation <dtcreation3d@icloud.com>
2021-03-12 18:29:38 +0100

    Add M73

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1191,7 +1191,7 @@
   #define SHOW_REMAINING_TIME       // Display estimated time to completion
   #if ENABLED(SHOW_REMAINING_TIME)
     #define USE_M73_REMAINING_TIME  // Use remaining time from M73 command instead of estimation
-    //#define ROTATE_PROGRESS_DISPLAY // Display (P)rogress, (E)lapsed, and (R)emaining time
+    #define ROTATE_PROGRESS_DISPLAY // Display (P)rogress, (E)lapsed, and (R)emaining time
   #endif
 
   #if EITHER(HAS_MARLINUI_U8GLIB, EXTENSIBLE_UI)
