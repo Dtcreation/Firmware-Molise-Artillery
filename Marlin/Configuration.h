5345734be5b31b1899849890be0f573cfd31a703
Dtcreation 3D <dtcreation3D@icloud.com>
2021-07-14 18:26:08 +0200

    Slow Z probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1358,7 +1358,7 @@
 #define XY_PROBE_FEEDRATE (133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST (20*60)
+#define Z_PROBE_FEEDRATE_FAST (10*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
